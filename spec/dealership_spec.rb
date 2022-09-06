44821a045ae21b78caa80e4964c8b002cb85cf8a
Aether Kerstens <philippekerstens@gmail.com>
2022-09-06 16:48:29 -0700

    Finish iteration 4 in dealership spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Dealership do
   end
 
   describe '#inventory_count' do
-    it '#can count cars' do
+    it 'can count cars' do
       dealership = Dealership.new("Acme Auto", "123 Main Street")
       inventory = []
       expect(dealership.inventory_count).to eq(0)
@@ -64,7 +64,7 @@ RSpec.describe Dealership do
   #   end
   # end
   describe '#total_value' do
-    it '#sums up all the car values' do
+    it 'sums up all the car values' do
       dealership = Dealership.new("Acme Auto", "123 Main Street")
       car_1 = Car.new("Ford Mustang", 1500, 36)
       car_2 = Car.new("Toyota Prius", 1000, 48)
@@ -79,7 +79,7 @@ RSpec.describe Dealership do
     end
   end
   describe '#details' do
-    it '#prints all the cars in the inventory' do
+    it 'prints all the cars in the inventory' do
       dealership = Dealership.new("Acme Auto", "123 Main Street")
       car_1 = Car.new("Ford Mustang", 1500, 36)
       car_2 = Car.new("Toyota Prius", 1000, 48)
@@ -92,4 +92,33 @@ RSpec.describe Dealership do
       dealership.details
     end
   end
+  describe '#average_price_of_car' do
+    it 'creates average price of all cars' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.average_price_of_car).to eq(39000)
+    end
+  end
+  # describe '#cars_sorted_by_price' do
+  #   it 'can arrange cars by price' do
+  #     dealership = Dealership.new("Acme Auto", "123 Main Street")
+  #     car_1 = Car.new("Ford Mustang", 1500, 36)
+  #     car_2 = Car.new("Toyota Prius", 1000, 48)
+  #     car_3 = Car.new("Toyota Tercel", 500, 48)
+  #     car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+  #     dealership.add_car(car_1)
+  #     dealership.add_car(car_2)
+  #     dealership.add_car(car_3)
+  #     dealership.add_car(car_4)
+  #     binding.pry
+  #     expect(dealership.cars_sorted_by_price).to eq()
+  #   end
+  # end
 end
