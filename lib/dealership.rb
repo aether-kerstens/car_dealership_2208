e4ae7abca4d4a3bc694ac021bed34be5b7cef5b4
Aether Kerstens <philippekerstens@gmail.com>
2022-09-06 15:54:32 -0700

    Finish and write iteration 3

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,5 +1,5 @@
+require './lib/car'
 class Dealership
-
   attr_reader :brand, :address, :inventory
 
   def initialize(brand, address)
@@ -8,15 +8,31 @@ class Dealership
     @inventory = []
   end
 
-  def inventory
-    puts @inventory
-  end
-
   def inventory_count
     @inventory.length
   end
 
   def add_car(car)
     @inventory << car
+  end
+
+  def has_inventory?
+    if @inventory.length == 0
+      false
+    else
+      true
+    end
+  end
+
+  # def cars_by_make(make)
+  #   inventory.map{|car| car.make}
+  # end
+
+  def total_value
+    inventory.sum{|car| car.total_cost}
+  end
+
+  def details
+    @inventory
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -39,4 +39,29 @@ RSpec.describe Dealership do
       expect(dealership.inventory_count).to eq(2)
     end
   end
+
+  describe '#has_inventory?' do
+    it 'can check inventory for cars' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      dealership.has_inventory?
+      expect(dealeship.has_inventory?).to eq(false)
+    end
+  end
+  describe '#cars_by_make' do
+    it '#groups cars by make' do
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      dealership.has_inventory?
+      dealership.cars_by_make("Toyota")
+      dealership.cars_by_make("Ford")
+      dealership.total_value
+      dealership.details
+    end
+  end
 end
